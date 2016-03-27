25de2da681ff3da62b973e790da2eddd5dac3125
Stephan Salat <stephan.salat@googlemail.com>
2016-03-27 12:26:51 +0200

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,29 @@
 var path = require('path');
+var webpack = require('webpack');
+
 var config = {
-    context: __dirname + "/src",
-	entry: [
-		'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-		'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-		"./main.js",
-	],
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist/js",
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015']
-                }				
-            },
-			{
-				test: /\.jsx?$/,
-				loaders: ['react-hot', 'jsx?harmony'],
-				include: path.join(__dirname, 'src')
-			},
-			{
-				test: /\.css$/,
-				loader: "style-loader!css-loader"
-			},
-			{
-				test: /\.css$/,
-				exclude: /\.useable\.css$/,
-				loader: "style!css"
-			},
-			{
-				test: /\.useable\.css$/,
-				loader: "style/useable!css"			
-			}			
-        ],
-    }
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/web/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src'),
+      exclude: /node_modules/,
+    }]
+  }
 };
+
 module.exports = config;
